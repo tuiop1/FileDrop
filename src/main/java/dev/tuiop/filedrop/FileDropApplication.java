9264499934e0b1eb15f1320ae420f92649249c68
tuiop1 <tymurkurkov@gmail.com>
2026-08-10 20:45:24 +0200

    Initial commit

---
+++ src/main/java/dev/tuiop/filedrop/FileDropApplication.java
@@ -0,0 +1,13 @@
+package dev.tuiop.filedrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tuiop/filedrop/FileDropApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tuiop.filedrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
